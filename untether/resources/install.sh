7930cf44d47d00e08e02a60a85df8a3b42b20839
staturnzz <118011249+staturnzz@users.noreply.github.com>
2026-03-09 08:53:01 -0500

    use aquila untether

--- untether/resources/install.sh
+++ untether/resources/install.sh
@@ -17,16 +17,15 @@ mv System/Library/LaunchDaemons/* Library/LaunchDaemons
 mv Library/LaunchDaemons/com.apple.CrashHousekeeping.plist System/Library/LaunchDaemons
 mv Library/LaunchDaemons/com.apple.MobileFileIntegrity.plist System/Library/LaunchDaemons
 mv Library/LaunchDaemons/com.apple.jetsamproperties.*.plist System/Library/LaunchDaemons
-mv Library/LaunchDaemons/com.apple.mDNSResponder.plist System/Library/LaunchDaemons
 
 mv Library/LaunchDaemons/com.apple.mobile.softwareupdated.plist System/Library/LaunchDaemons/com.apple.mobile.softwareupdated.plist.backup
 mv Library/LaunchDaemons/com.apple.softwareupdateservicesd.plist System/Library/LaunchDaemons/com.apple.softwareupdateservicesd.plist.backup
 
 mv -v usr/libexec/CrashHousekeeping usr/libexec/CrashHousekeeping.backup
-ln -s /lyncis usr/libexec/CrashHousekeeping
+ln -s /aquila usr/libexec/CrashHousekeeping
 
-chmod 0777 lyncis
-chown 0:0 lyncis
+chmod 0777 aquila
+chown 0:0 aquila
 chmod 0755 usr/libexec/CrashHousekeeping
 chown 0:0 usr/libexec/CrashHousekeeping
 
